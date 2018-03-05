b17f8e810b2141d3817fe9883e49d46237ead384
slisaasquatch <sli@saasquat.ch>
2018-03-05 14:14:40 -0800

    SL: added empty controller methods

--- app/skillstest/controllers/HomeController.java
+++ app/skillstest/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package controllers;
+package skillstest.controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -12,6 +12,17 @@ public class HomeController extends Controller {
     public Result index() {
         return status(200, "{\"message\":\"Hello world!\"}")
         		.as("application/json");
+    }
+
+    public Result parseTimeframe(String timeframe, String timeZone) {
+    	/*
+    	 * TODO parse the timeframe and return something like this:
+    	 * {
+    	 *   "from": "2018-01-01T22:12:41.174Z"
+    	 *   "to": "2018-01-02T22:12:41.174Z"
+    	 * }
+    	 */
+    	return status(501);
     }
 
 }
---
+++ app/skillstest/util/RelativeTimeframe.java
@@ -0,0 +1,15 @@
+package skillstest.util;
+
+public class RelativeTimeframe {
+
+	public Object toIntervalRelativeTo(long epochMilli, String timeZone) {
+		// TODO change return type to whatever you see fit and implement this method
+		return null;
+	}
+
+	public RelativeTimeframe parse(String s) {
+		// TODO implement parsing
+		return null;
+	}
+
+}
